b800e7b765cd3c640455b4248c7f86189b660882
Evgeniy Kovalskiy <49897130+PreVory@users.noreply.github.com>
2022-12-04 18:23:59 +0400

    Добавлена логика заполненения дней, которые были пропущены. Если был выбран только день 1, а после 10, будут заполнены 1-10.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,9 +30,9 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   List<int> containerList = List.generate(100, (index) => index + 1);
   List<int> pressedItems = [];
+  int focusedIndex = 0;
 
   late AnimationController controller;
-
 
   @override
   void initState() {
@@ -41,49 +40,54 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
       vsync: this,
       duration: const Duration(seconds: 7),
     )..addListener(() {
-      setState(() {});
-    });
+        setState(() {});
+      });
 
     super.initState();
   }
 
   @override
   void dispose() {
-
     controller.dispose();
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-
-    return
-      Stack(
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: Padding(
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: GridView.builder(
               itemCount: containerList.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5
-              ),
+                  crossAxisCount: 4, crossAxisSpacing: 5, mainAxisSpacing: 5),
               itemBuilder: (context, index) {
-                index = index++;
+                index = index + 1;
                 return GestureDetector(
                   onTap: () {
                     setState(() {
-                      if (pressedItems.contains(index) && pressedItems.last == index) {
-                        pressedItems.remove(index);
+                      focusedIndex = index;
+                      if (pressedItems.contains(index)) {
+                        List<int> toRemove = [];
+                        for (int i in pressedItems) {
+                          if (i > index) {
+                            toRemove.add(i);
+                          }
+                        }
+                        pressedItems.removeWhere((e) => toRemove.contains(e));
                       } else {
-                        pressedItems.add(index);
+                        for (int i = 1; i <= index; i++) {
+                          if (!(pressedItems.contains(i))) {
+                            pressedItems.add(i);
+                          }
+                        }
                       }
-                      controller.animateTo(pressedItems.isNotEmpty ? (pressedItems.last + 0.01) / 100 : 0);
-
+                      controller.animateTo(pressedItems.isNotEmpty
+                          ? (pressedItems.last + 0.01) / 100
+                          : 0);
                     });
                   },
                   child: Container(
@@ -96,35 +100,43 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
                     height: 50,
                     child: Center(
                         child: Text(
-                            (index+1).toString(),
+                      (index).toString(),
                     )),
                   ),
                 );
               },
             ),
+          ),
         ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-              child: Container(
-                decoration: BoxDecoration(color: Colors.grey,),
-                height: 200,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    LinearProgressIndicator(value: controller.value, color: Colors.green, minHeight: 7),
-                    ElevatedButton(onPressed: () {
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+            ),
+            height: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                LinearProgressIndicator(
+                    value: controller.value, color: Colors.green, minHeight: 7),
+                ElevatedButton(
+                    onPressed: () {
                       setState(() {
+                        focusedIndex = 0;
                         pressedItems.clear();
-                        controller.animateTo(pressedItems.isNotEmpty ? (pressedItems.last + 0.01) / 100 : 0);
+                        controller.animateTo(pressedItems.isNotEmpty
+                            ? (pressedItems.last + 0.01) / 100
+                            : 0);
                       });
-
-                    }, child: const Text('Clear')),
-                  ],
-                ),
-              ),
+                    },
+                    child: const Text('Clear')),
+                Text('$focusedIndex из 100'),
+              ],
+            ),
           ),
-        ],
-      );
+        ),
+      ],
+    );
   }
 }
